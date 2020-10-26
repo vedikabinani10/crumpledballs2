193eb1b72c83e22ef0921a97636098fb2ace78fd
vedikabinani10 <69752144+vedikabinani10@users.noreply.github.com>
2020-10-26 21:16:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,39 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const matter = Matter.Bodies;
 const Body = Matter.Body;
+
+var engine,world;
 var paperball;
 var dustbin;
 var ground;
 
-function preload()
-{
-	
-}
-
 function setup() {
-	var canvas = createCanvas(800, 700);
+	createCanvas(800, 700);
 	
 	engine = Engine.create();
 	world = engine.world;
-
-	paperball = new Paper(100,550);
-
 	dustbin = new Dustbin(550,630,130,150);
-
+	paperball = new Paper(100,550);
 	ground = new Ground(400,690,800,20);
-
-
 }
 
 
 function draw() {
-  rectMode(CENTER);
   Engine.update(engine);
   background(200);
   
   paperball.display();
   ground.display();
   dustbin.display();
-  
-
-  keyPressed();
-  drawSprites();
 }
 
 function keyPressed(){
-	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:5,y:-5});
+	if(keyCode===38){
+		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:200,y:-100});
+	}
+	if(keyCode===40){
+		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:0,y:500});
 	}
 	
 }
